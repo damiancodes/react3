3e0531d7e52f0bb5209754ddd0f1c613506c4328
damiancodes <damianbwire88@gmail.com>
2025-05-05 16:03:20 +0300

    commit

---
+++ src/Pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+export const Contact = () => {
+  return (
+    <div>
+          <Navbar />
+        <h1>Contact</h1>
+    </div>
+  )
+}
